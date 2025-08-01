0a633e6643543b35c79a7bd0498bd400e449e43b
Ludovic <ludovicdebever0@gmail.com>
2025-08-01 15:43:58 +0200

    fix(config): fix tuning params
    Bench: 306593

--- engine/src/config.rs
+++ engine/src/config.rs
@@ -45,6 +45,7 @@ pub struct OptionDescriptor<T> {
     pub value: T,
     pub min: T,
     pub max: T,
+    pub tunable: bool,
 }
 
 #[cfg(feature = "tuning")]
@@ -88,7 +89,7 @@ macro_rules! impl_fmt_display {
 impl_fmt_display!(i32, u8, usize, i64, u64, f64);
 
 macro_rules! make_config {
-    ($($field:ident: $t:ty = ($name:expr, $kind:expr, $val:expr, $min:expr, $max:expr);)*) => {
+    ($($field:ident: $t:ty = ($name:expr, $kind:expr, $val:expr, $min:expr, $max:expr, $tunable:expr);)*) => {
         pub struct Config {
             $(pub $field: OptionDescriptor<$t>,)*
         }
@@ -103,17 +104,27 @@ macro_rules! make_config {
                             value: $val,
                             min: $min,
                             max: $max,
+                            tunable: $tunable,
                         },
                     )*
                 }
             }
 
-            pub fn set(&mut self, name: &str, val: i64) {
+            pub fn set(&mut self, name: &str, val: &str) {
                 match name {
                     $(
-                        $name => self.$field.value = val as $t,
+                        $name => {
+                            if !cfg!(feature = "tuning") && self.$field.tunable {
+                                Logger::log(&format!("info string cannot modify tunable option in non tunable build"))
+                            }
+                            else if let Ok(parsed) = val.parse::<$t>() {
+                                self.$field.value = parsed;
+                            } else {
+                                Logger::log(&format!("info string invalid value for {}: {}", $name, val));
+                            }
+                        },
                     )*
-                    _ => Logger::log(format!("info string unknown option: {}", name).as_str()),
+                    _ => Logger::log(&format!("info string unknown option: {}", name)),
                 }
             }
 
@@ -126,7 +137,10 @@ macro_rules! make_config {
 
             pub fn print_uci_options(&self) {
                 $(
-                    Logger::log(format!("{}", self.$field).as_str());
+
+                    if cfg!(feature = "tuning") || !self.$field.tunable {
+                        Logger::log(format!("{}", self.$field).as_str());
+                    }
                 )*
             }
         }
@@ -134,31 +148,31 @@ macro_rules! make_config {
 }
 
 make_config! {
-    move_overhead: usize = ("MoveOverhead", OptionKind::Spin, 0, 0, 10000);
-    threads: u8 = ("Threads", OptionKind::Spin, 1, 1, 1);
-    hash: usize = ("Hash", OptionKind::Spin, 255, 1, 1024);
-    qsearch_depth: u8 = ("QSearchDepth", OptionKind::Spin, 17, 1, 20);
-    rfp_depth: u8 = ("RFPDepth", OptionKind::Spin, 11, 1, 20);
-    rfp_base_margin: i32 = ("RFPBaseMargin", OptionKind::Spin, 57, 1, 200);
-    rfp_reduction_improving: i32 = ("RFPReductionImproving", OptionKind::Spin, 24, 1, 200);
-    fp_depth_margin: u8 = ("FPDepthMargin", OptionKind::Spin, 5, 1, 20);
-    fp_base_margin: i32 = ("FPBaseMargin", OptionKind::Spin, 40, 1, 200);
-    fp_margin_depth_factor: i32 = ("FPMarginDepthFactor", OptionKind::Spin, 35, 1, 200);
-    nmp_depth: u8 = ("NMPDepth", OptionKind::Spin, 5, 1, 20);
-    nmp_margin: u8 = ("NMPMargin", OptionKind::Spin, 7, 1, 20);
-    nmp_divisor: u8 = ("NMPDivisor", OptionKind::Spin, 2, 1, 20);
-    nmp_divisor_improving: u8 = ("NMPDivisorImproving", OptionKind::Spin, 10, 1, 20);
-    lmp_move_margin: usize = ("LMPMoveMargin", OptionKind::Spin, 5, 1, 20);
-    lmp_depth_factor: u8 = ("LMPDepthFactor", OptionKind::Spin, 7, 1, 20);
-    lmr_depth: u8 = ("LMRDepth", OptionKind::Spin, 15, 1, 20);
-    lmr_move_margin: usize = ("LMRMoveMargin", OptionKind::Spin, 5, 1, 20);
-    lmr_quiet_margin: f64 = ("LMRQuietMargin", OptionKind::String, 2.59, 0.0, 10.0);
-    lmr_quiet_divisor: f64 = ("LMRQuietDivisor", OptionKind::String, 2.02, 1.0, 10.0);
-    lmr_base_margin: f64 = ("LMRBaseMargin", OptionKind::String, 1.31, 0.0, 10.0);
-    lmr_base_divisor: f64 = ("LMRBaseDivisor", OptionKind::String, 3.28, 1.0, 10.0);
-    mo_tt_entry_value: i32 = ("MOTTEntryValue", OptionKind::Spin, 229, 1, 500);
-    mo_capture_value: i32 = ("MOCaptureValue", OptionKind::Spin, 55, 0, 500);
-    mo_killer_value: i32 = ("MOKillerValue", OptionKind::Spin, 78, 0, 500);
-    tc_time_divisor: u64 = ("TCTimeDivisor", OptionKind::Spin, 2, 2, 100);
-    tc_elapsed_factor: i64 = ("TCElapsedFactor", OptionKind::Spin, 8, 1, 10);
+    move_overhead: usize = ("MoveOverhead", OptionKind::Spin, 0, 0, 10000, false);
+    threads: u8 = ("Threads", OptionKind::Spin, 1, 1, 1, false);
+    hash: usize = ("Hash", OptionKind::Spin, 255, 1, 1024, false);
+    qsearch_depth: u8 = ("QSearchDepth", OptionKind::Spin, 17, 1, 20, true);
+    rfp_depth: u8 = ("RFPDepth", OptionKind::Spin, 11, 1, 20, true);
+    rfp_base_margin: i32 = ("RFPBaseMargin", OptionKind::Spin, 57, 1, 200, true);
+    rfp_reduction_improving: i32 = ("RFPReductionImproving", OptionKind::Spin, 24, 1, 200, true);
+    fp_depth_margin: u8 = ("FPDepthMargin", OptionKind::Spin, 5, 1, 20, true);
+    fp_base_margin: i32 = ("FPBaseMargin", OptionKind::Spin, 40, 1, 200, true);
+    fp_margin_depth_factor: i32 = ("FPMarginDepthFactor", OptionKind::Spin, 35, 1, 200, true);
+    nmp_depth: u8 = ("NMPDepth", OptionKind::Spin, 5, 1, 20, true);
+    nmp_margin: u8 = ("NMPMargin", OptionKind::Spin, 7, 1, 20, true);
+    nmp_divisor: u8 = ("NMPDivisor",OptionKind::Spin, 2, 1, 20, true);
+    nmp_divisor_improving: u8 = ("NMPDivisorImproving", OptionKind::Spin, 10, 1, 20, true);
+    lmp_move_margin: usize = ("LMPMoveMargin", OptionKind::Spin, 5, 1, 20, true);
+    lmp_depth_factor: u8 = ("LMPDepthFactor", OptionKind::Spin, 7, 1, 20, true);
+    lmr_depth: u8 = ("LMRDepth", OptionKind::Spin, 15, 1, 20, true);
+    lmr_move_margin: usize = ("LMRMoveMargin", OptionKind::Spin, 5, 1, 20, true);
+    lmr_quiet_margin: f64 = ("LMRQuietMargin", OptionKind::String, 2.59, 0.0, 10.0, true);
+    lmr_quiet_divisor: f64 = ("LMRQuietDivisor", OptionKind::String, 2.02, 1.0, 10.0, true);
+    lmr_base_margin: f64 = ("LMRBaseMargin", OptionKind::String, 1.31, 0.0, 10.0, true);
+    lmr_base_divisor: f64 = ("LMRBaseDivisor", OptionKind::String, 3.28, 1.0, 10.0, true);
+    mo_tt_entry_value: i32 = ("MOTTEntryValue", OptionKind::Spin, 229, 1, 500, true);
+    mo_capture_value: i32 = ("MOCaptureValue", OptionKind::Spin, 55, 0, 500, true);
+    mo_killer_value: i32 = ("MOKillerValue", OptionKind::Spin, 78, 0, 500, true);
+    tc_time_divisor: u64 = ("TCTimeDivisor", OptionKind::Spin, 2, 2, 100, true);
+    tc_elapsed_factor: i64 = ("TCElapsedFactor", OptionKind::Spin, 8, 1, 10, true);
 }
