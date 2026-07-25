394e9a98f757d3d1acb6e83cd2e0ca9926ae75c7
Ludovic <ludovic.debever@holis.earth>
2026-07-26 00:46:46 +0200

    fix(uci): add wtime & btime + fix uci out + fix stop
        bench 2402

--- src/search/time.rs
+++ src/search/time.rs
@@ -1,10 +1,11 @@
+use crate::search::search_options::BuiltSearchOptions;
 use std::time::SystemTime;
 
 pub struct TimeManager;
 
 impl TimeManager {
-    pub(crate) fn should_stop(start_time: SystemTime, move_time: Option<u64>) -> bool {
-        if let Some(move_time) = move_time {
+    pub(crate) fn should_stop(start_time: SystemTime, opt: &BuiltSearchOptions) -> bool {
+        if let Some(move_time) = opt.time {
             let elapsed_time = SystemTime::now()
                 .duration_since(start_time)
                 .unwrap()
